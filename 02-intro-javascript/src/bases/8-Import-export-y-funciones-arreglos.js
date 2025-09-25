b2f0487dcb8cd49b27ed183cdcdb70e243eb6eee
Chars <chars24@gmail.com>
2025-09-25 13:45:08 -0600

    02-intro-javascript(import export y funciones comundes de arreglos)
    feat: Implementa importación de datos y funciones de arreglos
    
    En esta lección, avanzamos en la modularización de nuestro código y en el manejo de datos con funciones de arreglos.
    
    Puntos clave de la lección:
    - Se introduce el uso de `import` para traer datos desde otros archivos, promoviendo un código más limpio y organizado.
    - Se aplican las funciones `find` y `filter` para realizar búsquedas específicas dentro de un arreglo de objetos.
    - Se crean funciones (`geHeroeById`, `getHeroresByOwner`) que encapsulan la lógica de búsqueda, un principio clave para la reutilización de código.

--- 02-intro-javascript/src/bases/8-Import-export-y-funciones-arreglos.js
+++ 02-intro-javascript/src/bases/8-Import-export-y-funciones-arreglos.js
@@ -1,11 +1,13 @@
-import { heroes } from "./data/heroes";
+import { heroes } from "../data/heroes";
 
+// find
 const geHeroeById = (id) => {
   return heroes.find((heroe) => heroe.id === id);
 };
 
 console.log(geHeroeById(1));
 
+// filter
 const getHeroresByOwner = (owner) => {
   return heroes.filter((heroe) => heroe.owner === owner);
 };
