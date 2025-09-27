4f3adfc8772103a20fc067b3a2887c736125953a
Chars <chars24@gmail.com>
2025-09-27 11:48:10 -0600

    02-Intro-Javascript(promesas)
    feat: Implementa Promesas para la obtención asíncrona de héroes
    
    Este commit introduce el uso de Promesas de JavaScript para manejar operaciones asíncronas al obtener datos de héroes.
    
    - Se ha creado una nueva función, `getHeroeByIdAsync`. Devuelve una Promesa que simula una petición de red usando `setTimeout`.
    - La Promesa se resuelve con el objeto del héroe si se encuentra por su ID.
    - Si no se encuentra el héroe, la Promesa se rechaza con un mensaje de error.
    - Se utilizan los métodos `.then()` y `.catch()` para manejar la resolución exitosa y el posible rechazo de la Promesa, respectivamente.

--- 02-intro-javascript/src/bases/09-Multiples exportaciones y exportaciones por defecto.js
+++ 02-intro-javascript/src/bases/09-Multiples exportaciones y exportaciones por defecto.js
@@ -1,15 +1,15 @@
-import heroes, { owners } from "./data/heroes";
+import heroes, { owners } from "../data/heroes";
 
-console.log(owners);
+// console.log(owners);
 
-const geHeroeById = (id) => {
+export const geHeroeById = (id) => {
   return heroes.find((heroe) => heroe.id === id);
 };
 
-console.log(geHeroeById(1));
+// console.log(geHeroeById(1));
 
-const getHeroresByOwner = (owner) => {
+export const getHeroresByOwner = (owner) => {
   return heroes.filter((heroe) => heroe.owner === owner);
 };
 
-console.log(getHeroresByOwner("DC"));
+// console.log(getHeroresByOwner("DC"));
