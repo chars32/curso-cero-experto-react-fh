b97fe3b0484ef52f4d140d5b63c94b00411871d9
Chars <chars24@gmail.com>
2025-09-27 13:50:17 -0600

    02-intro-javascript(operador condicional ternario)
    feat: Demuestra el uso del operador lógico AND para condicionales
    
    Este commit ilustra una forma concisa de realizar asignaciones condicionales utilizando el operador lógico AND (`&&`) como cortocircuito.
    
    - Se muestra cómo el operador `&&` puede ser una alternativa al operador ternario, especialmente cuando solo se necesita evaluar la condición verdadera.
    - Si la primera expresión es `false`, la evaluación se detiene y devuelve `false`. Si es `true`, devuelve el valor de la segunda expresión.
    - Este patrón es comúnmente utilizado en JavaScript moderno para renderizado condicional y asignaciones breves.
    - Tambien usamos el operador ternario ?

--- 02-intro-javascript/src/bases/13-operador-condicional-ternario.js
+++ 02-intro-javascript/src/bases/13-operador-condicional-ternario.js
@@ -1,6 +1,6 @@
 const activo = true;
 
-// const mensaje = activo ? "Activo" : "Inactivo"; (operador ternario)
+// const mensaje = activo ? "Activo" : "Inactivo";
 const mensaje = !activo && "Activo";
 
 console.log(mensaje);
