8ca00e778714d8670b6a8d6a1c6c0991be7ef751
Chars <chars24@gmail.com>
2025-09-28 23:34:42 -0600

    03-CSSPropoerties

--- 03-firsts-steps/src/MyAwesomeApp.tsx
+++ 03-firsts-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,16 @@
+import type { CSSProperties } from "react";
+
+const myStyles: CSSProperties = {
+  backgroundColor: "#fafafa",
+  borderRadius: 20,
+  padding: 10,
+};
+
 export function MyAwesomeApp() {
   return (
     <>
       <h1>Carlos</h1>
-      <h3>García</h3>
+      <h3 style={myStyles}>García</h3>
     </>
   );
 }
