f5213a2d534edf40fb7a80479fea253fe4fe1415
Chars <chars24@gmail.com>
2025-09-30 22:25:56 -0600

    5. Buscar elementos en el componente renderizado

--- 03-firsts-steps/src/MyAwesomeApp copy.test.tsx
+++ 03-firsts-steps/src/MyAwesomeApp copy.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
@@ -6,6 +6,23 @@ describe("MyAwesomeApp", () => {
   test("should render firstName and lastName", () => {
     const { container } = render(<MyAwesomeApp />);
 
+    // screen.debug();
+
+    const h1 = container.querySelector("h1");
+    expect(h1?.innerHTML).toContain("Carlos");
+
+    const h3 = container.querySelector("h3");
+    expect(h3?.innerHTML).toContain("García");
+  });
+
+  test("should render firstName and lastName - screen", () => {
+    render(<MyAwesomeApp />);
     screen.debug();
+
+    const h1 = screen.getByRole("heading", {
+      level: 1,
+    });
+
+    expect(h1.innerHTML).toContain("Carlos");
   });
 });
