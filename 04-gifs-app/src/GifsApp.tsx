02b9d114d6c514bd65fc681d4d89f1025fc7bc44
Chars <chars24@gmail.com>
2025-10-05 12:36:47 -0600

    6. Manejo de estado - Busquedas previas
    feat(gifs-app): Añade interactividad a las búsquedas previas
    
    Se ha refactorizado el componente `PreviousSearches` para que sea interactivo. Ahora, al hacer clic en uno de los términos de búsqueda anteriores, se emite un evento.
    
    - Se agregó la propiedad `onLabelClicked` al componente `PreviousSearches` para manejar el evento de clic.
    - Se movió la gestión de los términos de búsqueda al estado del componente principal `GifsApp` para un manejo dinámico.

--- 04-gifs-app/src/GifsApp.tsx
+++ 04-gifs-app/src/GifsApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
@@ -5,6 +6,12 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 
 export const GifsApp = () => {
+  const [previousTerms, setPreviousTerms] = useState(["Dragon Ball Z"]);
+
+  const handleTermClicked = (term: string) => {
+    console.log(term);
+  };
+
   return (
     <>
       {/* Heade   */}
@@ -17,7 +24,10 @@ export const GifsApp = () => {
       <SearchBar placeholder="Busca lo que quieras" buttonName="Buscar" />
 
       {/* Busquedas Previas */}
-      <PreviousSearches searches={["Goku", "Dragon Ball", "Demon Slayer"]} />
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
 
       {/* Gifs */}
       <GifList gifs={mockGifs} />
