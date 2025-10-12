0d22e786595fb27122341a424d89f17cd28c1231
Chars <chars24@gmail.com>
2025-10-12 09:04:25 -0600

    7. Problema que resuelve un "custom hook".
    feat(counter): Crea componente de contador con estado
    
    Se ha añadido un nuevo componente `MyCounterApp` que implementa una funcionalidad de contador básica.
    
    Este componente utiliza el hook `useState` para gestionar su estado interno, inicializado en 5. Proporciona una interfaz de usuario con tres botones que permiten al usuario:
    - Incrementar el contador en 1.
    - Decrementar el contador en 1.
    - Reiniciar el contador a su valor inicial.
    
    El componente está autocontenido y encapsula toda la lógica y la presentación del contador.

--- 04-gifs-app/src/main.tsx
+++ 04-gifs-app/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { GifsApp } from "./GifsApp";
+// import { GifsApp } from "./GifsApp";
 
 import "./index.css";
+import { MyCounterApp } from "./counter/components/MyCounterApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GifsApp />
+    {/* <GifsApp /> */}
+    <MyCounterApp />
   </StrictMode>
 );
