e55a51981490747386738a72dfc5377e5c33441c
Chars <chars24@gmail.com>
2025-10-12 08:46:28 -0600

    6. **Evitar multiples llamados al api (hasta este punto no viene en el curso)**
    feat(search): Optimiza la función de búsqueda con useCallback
    
    Se ha optimizado la función `handleSearch` en el componente `GifsApp` para evitar la generación de múltiples peticiones a la API.
    
    Anteriormente, la función `handleSearch` se recreaba en cada renderizado del componente. Al pasar esta nueva instancia como prop al componente `SearchBar`, se activaba un `useEffect` que dependía de dicha función, provocando un ciclo de peticiones no deseadas que podían llevar a un error 429 (Too Many Requests).
    
    Mediante el uso del hook `useCallback`, la función `handleSearch` ahora es memoizada. Esto garantiza que la referencia a la función se mantenga estable entre renderizados, a menos que sus dependencias (`previousTerms`) cambien. Como resultado, se previene la ejecución repetida del `useEffect` en `SearchBar`, solucionando el problema de las peticiones excesivas y mejorando el rendimiento de la aplicación.

--- 04-gifs-app/src/GifsApp.tsx
+++ 04-gifs-app/src/GifsApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { CustomHeader } from "./shared/components/CustomHeader";
@@ -14,23 +14,26 @@ export const GifsApp = () => {
     console.log(term);
   };
 
-  const handleSearch = async (query: string) => {
-    // Convertimos la query a minúsculas y eliminamos espacios en blanco al inicio
-    // y al final
-    query = query.trim().toLowerCase();
+  const handleSearch = useCallback(
+    async (query: string) => {
+      // Convertimos la query a minúsculas y eliminamos espacios en blanco al inicio
+      // y al final
+      query = query.trim().toLowerCase();
 
-    // Si la query está vacía, no hacemos nada
-    if (query.trim().length === 0) return;
+      // Si la query está vacía, no hacemos nada
+      if (query.trim().length === 0) return;
 
-    // Si la query no está en el array de términos previos, la añadimos al inicio
-    // y limitamos el array a 5 términos
-    if (!previousTerms.includes(query)) {
-      setPreviousTerms((prevTerms) => [query, ...prevTerms].splice(0, 5));
-    }
+      // Si la query no está en el array de términos previos, la añadimos al inicio
+      // y limitamos el array a 5 términos
+      if (!previousTerms.includes(query)) {
+        setPreviousTerms((prevTerms) => [query, ...prevTerms].splice(0, 5));
+      }
 
-    const gifs = await getGifsByQuery(query);
-    setGifs(gifs);
-  };
+      const gifs = await getGifsByQuery(query);
+      setGifs(gifs);
+    },
+    [previousTerms]
+  );
 
   return (
     <>
