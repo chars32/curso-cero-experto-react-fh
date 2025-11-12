968e0ddd09945afcea05810e3225bf3e028d6b62
Chars <chars24@gmail.com>
2025-11-11 20:01:48 -0600

    10. Efectos secundarios con un reducer.
    feat(ScrambleWords): Añadir efecto de confeti al ganar puntos
    
    Se ha mejorado la experiencia de usuario en el juego "Scramble Words" añadiendo un efecto visual de confeti cada vez que el jugador adivina una palabra correctamente y gana un punto.
    
    Cambios implementados:
    
    -   **Uso de `useEffect`:** Se ha introducido un `useEffect` en el componente `ScrambleWords.tsx` que se ejecuta cada vez que el estado `points` se actualiza.
    -   **Efecto de Confeti:** Dentro del `useEffect`, se llama a la función `confetti()` para mostrar una animación de celebración, proporcionando una retroalimentación visual inmediata y gratificante al jugador por su acierto.
    -   **Condición de Ejecución:** El efecto se dispara únicamente cuando el puntaje es mayor a cero, evitando que se muestre al iniciar el juego por primera vez.
    
    Este cambio añade un toque de dinamismo y recompensa visual al juego, mejorando la interacción y la satisfacción del usuario.

--- 05-hooks-app/src/05-useReducer/ScrambleWords.tsx
+++ 05-hooks-app/src/05-useReducer/ScrambleWords.tsx
@@ -2,7 +2,7 @@
 // Es necesario componentes de Shadcn/ui
 // https://ui.shadcn.com/docs/installation/vite
 
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
@@ -29,6 +29,16 @@ export const ScrambleWords = () => {
     skipCounter,
     totalWords,
   } = state;
+
+  useEffect(() => {
+    if (points === 0) return;
+
+    confetti({
+      particleCount: 100,
+      spread: 120,
+      origin: { y: 0.6 },
+    });
+  }, [points]);
 
   const handleGuessSubmit = (e: React.FormEvent) => {
     // Previene el refresh de la página
