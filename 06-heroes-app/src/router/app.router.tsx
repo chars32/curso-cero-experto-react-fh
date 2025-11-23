12c1ea12863554031c9ab8f3acb90f53ac04199d
Chars <chars24@gmail.com>
2025-11-23 15:55:34 -0600

    13. Lazy Load - Carga perezosa.
    Optimización: Implementación de Lazy Loading en SearchPage
    
    Se han realizado optimizaciones en la carga de rutas para mejorar el rendimiento de la aplicación:
    
    - **Router:** Se modificó `app.router.tsx` para importar `SearchPage` dinámicamente utilizando `React.lazy()`. Esto permite dividir el código (code splitting) y evitar que el componente de búsqueda se cargue en el bundle inicial.
    - **SearchPage:** Se añadió una exportación por defecto (`export default`) en `SearchPage.tsx` para hacer compatible el componente con la importación dinámica de React.

--- 06-heroes-app/src/router/app.router.tsx
+++ 06-heroes-app/src/router/app.router.tsx
@@ -4,8 +4,12 @@ import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminPageLayout } from "@/admin/layouts/AdminPageLayout";
+import { lazy } from "react";
+
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
